673e317be7528bd0f6d64d4353d112db4186cf30
shikodingo <shikodingo@gmail.com>
2015-12-15 09:21:28 -0500

    Added in basic clipping and randomizing functions to the project.

--- SlidePuzzleGame/SlidePuzzleGame/MainPage.xaml.cs
+++ SlidePuzzleGame/SlidePuzzleGame/MainPage.xaml.cs
@@ -17,6 +17,13 @@ using Windows.Storage;
 using Windows.Storage.Pickers;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.Graphics.Imaging;
+//using System.Windows.Media.Imaging;
+//using static System.Windows.Media.Imaging.CroppedBitmap;
+using Windows.Media.Capture;
+using System.Threading.Tasks;
+using Windows.UI;
+
+
 
 
 //using System.Windows.Controls;
@@ -31,11 +38,13 @@ namespace SlidePuzzleGame
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        Random rnd;
         public MainPage()
         {
             // This keeps the information on the page saved
             this.NavigationCacheMode = NavigationCacheMode.Enabled;
             this.InitializeComponent();
+            rnd = new Random();
         }
 
         private async void btnGetPicture_Click(object sender, RoutedEventArgs e)
@@ -49,29 +58,102 @@ namespace SlidePuzzleGame
             StorageFile file = await openPicker.PickSingleFileAsync();
             if (file != null)
             {
-                // Application now has read/write access to the picked file
-                //OutputTextBlock.Text = "Picked photo: " + file.Name;
+                BitmapImage btmp = await SetImageSource(file);
+                ImageBrush image = new ImageBrush();
+
+                cutImage(btmp);
+               // image.ImageSource = btmp;
+
+                //MainCanvas.Background = image;
             }
             else
-            { 
-               // OutputTextBlock.Text = "Operation cancelled.";
+            {
+                txtError.Text = "Could not open the specified file.";
             }
-
-            //http://stackoverflow.com/questions/8881865/saving-a-wpf-canvas-as-an-image
-           // RenderTargetBitmap rtb = new RenderTargetBitmap((int)MainCanvas.RenderSize.Width,
-           //(int)MainCanvas.RenderSize.Height, 96d, 96d, System.Windows.Media.PixelFormats.Default);
-           // await rtb.RenderAsync(MainCanvas);
-           // //rtb.Render(MainCanvas);
-
-           // var crop = new CroppedBitmap(rtb, new Int32Rect(50, 50, 250, 250));
-
-           // BitmapEncoder pngEncoder = new PngBitmapEncoder();
-           // pngEncoder.Frames.Add(BitmapFrame.Create(crop));
-
-           // using (var fs = File.OpenWrite("logo.png"))
-           // {
-           //     pngEncoder.Save(fs);
-           // }
         }
+
+        public void cutImage(BitmapImage image)
+        {
+            //http://www.c-sharpcorner.com/uploadfile/mahesh/clipping-or-cropping-images-in-wpf/
+            // Clipped Image
+            int count = 0;
+            Image clippedImage = new Image();
+            clippedImage.Source = image;
+            RectangleGeometry clipGeometry = new RectangleGeometry();
+            for(count = 1; count <= 16; count++)
+            {
+                Rect piece = new Rect();
+                int randX = rnd.Next(50, Convert.ToInt32(MainCanvas.ActualWidth - (1 + 150)));
+                int randY = rnd.Next(50, Convert.ToInt32(MainCanvas.ActualHeight - (1 + 100)));
+                piece.X = randX;
+                piece.Y = randY;
+                piece.Width = 150;
+                piece.Height = 100;
+                clipGeometry.Rect = piece;
+               // clipGeometry.Rect = new Rect(200, 200, 100, 100);
+                clippedImage.Clip = clipGeometry;
+                // Need to call random placement function here as it probably just places each square in one place.
+            }
+            MainCanvas.Children.Add(clippedImage);
+
+            //MainCanvas.Background = clippedImage;
+            // LayoutRoot.Children.Add(clippedImage);
+        }
+
+        private async Task<BitmapImage> SetImageSource(StorageFile file)
+        {
+            //var file = await
+            //  Windows.Storage.KnownFolders.PicturesLibrary.GetFileAsync(fileName);
+            var stream = await file.OpenReadAsync();
+            var bitmapImage = new Windows.UI.Xaml.Media.Imaging.BitmapImage();
+            bitmapImage.SetSource(stream);
+
+            return bitmapImage;
+        }
+
+        public async void openCamera()
+        {
+            // Using Windows.Media.Capture.CameraCaptureUI API to capture a photo
+            CameraCaptureUI dialog = new CameraCaptureUI();
+            Size aspectRatio = new Size(16, 9);
+            dialog.PhotoSettings.CroppedAspectRatio = aspectRatio;
+
+            StorageFile file = await dialog.CaptureFileAsync(CameraCaptureUIMode.Photo);
+            BitmapImage test = await SetImageSource(file);
+            ImageBrush image = new ImageBrush();
+            image.ImageSource = test;
+
+            
+            MainCanvas.Background = image;
+        }
+
+        private void btnCamera_Click(object sender, RoutedEventArgs e)
+        {
+            openCamera();
+        }
+
+        private void btnRestart_Click(object sender, RoutedEventArgs e)
+        {
+            // This function should actually restart the positions of the image blocks
+            MainCanvas.Children.Clear();
+            //MainCanvas.Children.Remove<Image>();
+        }
+
+        //http://stackoverflow.com/questions/8881865/saving-a-wpf-canvas-as-an-image
+        // RenderTargetBitmap rtb = new RenderTargetBitmap((int)MainCanvas.RenderSize.Width,
+        //(int)MainCanvas.RenderSize.Height, 96d, 96d, System.Windows.Media.PixelFormats.Default);
+        // await rtb.RenderAsync(MainCanvas);
+        // //rtb.Render(MainCanvas);
+
+        // var crop = new CroppedBitmap(rtb, new Int32Rect(50, 50, 250, 250));
+
+        // BitmapEncoder pngEncoder = new PngBitmapEncoder();
+        // pngEncoder.Frames.Add(BitmapFrame.Create(crop));
+
+        // using (var fs = File.OpenWrite("logo.png"))
+        // {
+        //     pngEncoder.Save(fs);
+        // }
+
     }
 }
